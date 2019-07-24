f5e5c96ecf37b62f54c9b1f94e3d9772b93d898a
nwilliamfeng <nwilliamfeng@163.com>
2019-07-24 18:00:57 +0800

    commit 190724a

---
+++ Demo/SettingDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// SettingDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingDialog : Window
+    {
+        public SettingDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void closeBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.Red;
+        }
+    }
+}
